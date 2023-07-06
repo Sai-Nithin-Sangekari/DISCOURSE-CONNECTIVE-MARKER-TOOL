98d40216349e44e2cde8cd2394f22b2762082fc9
FatemaBawahir <bawahir.fatema@gmail.com>
2023-07-06 16:18:30 +0530

    Revert "USR"
    This reverts commit 067c6b8291ed7feee01e310c3666e85853b7274d.

--- USR_generator/CONSTANTS.py
+++
@@ -1,3 +0,0 @@
-INPUT_FILE = "/Users/fatema/Desktop/workspace/LanguageCommunicator/AutomaticUSRGenerator/USR_generator/input.txt"
-PARSER_OUTPUT_FILE = "/Users/fatema/Desktop/workspace/LanguageCommunicator/AutomaticUSRGenerator/USR_generator/output.txt"
-PROCESSED_PARSER_OUTPUT_FILE = "/Users/fatema/Desktop/workspace/LanguageCommunicator/AutomaticUSRGenerator/USR_generator/parser_output.txt"
